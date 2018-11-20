26a361f3bb77ef4ad6bdec6778aee444cb08b7ea
Jose Luis Martinez <ivonnics@protonmail.com>
2018-11-20 11:42:03 -0500

    Add files via upload

---
+++ Preparando la data Github.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 20 10:57:40 2018
+
+@author: ivonnics
+"""
+
+# Rescale data (between 0 and 1)
+import pandas
+import numpy
+from sklearn.preprocessing import MinMaxScaler
+url = "https://github.com/ivonnics/Machine-Learning/blob/master/Indians-diabetes.data.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = pandas.read_html(url)
+Tabla=dataframe[0]
+dataframe=Tabla[['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']]
+array = dataframe.values
+print(dataframe.head(5))
+# separate array into input and output components
+X = array[:,0:8]
+Y = array[:,8]
+scaler = MinMaxScaler(feature_range=(0, 1))
+rescaledX = scaler.fit_transform(X)
+# summarize transformed data
+numpy.set_printoptions(precision=3)
+print(rescaledX[0:5,:])
+
+
+# Standardize data (0 mean, 1 stdev)
+from sklearn.preprocessing import StandardScaler
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = pandas.read_csv(url, names=names)
+array = dataframe.values
+# separate array into input and output components
+X = array[:,0:8]
+Y = array[:,8]
+scaler = StandardScaler().fit(X)
+rescaledX = scaler.transform(X)
+# summarize transformed data
+numpy.set_printoptions(precision=3)
+print(rescaledX[0:5,:])
+
+
+# Normalize data (length of 1)
+from sklearn.preprocessing import Normalizer
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = pandas.read_csv(url, names=names)
+array = dataframe.values
+# separate array into input and output components
+X = array[:,0:8]
+Y = array[:,8]
+scaler = Normalizer().fit(X)
+normalizedX = scaler.transform(X)
+# summarize transformed data
+numpy.set_printoptions(precision=3)
+print(normalizedX[0:5,:])
+
+
+# binarization
+from sklearn.preprocessing import Binarizer
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+dataframe = pandas.read_csv(url, names=names)
+array = dataframe.values
+# separate array into input and output components
+X = array[:,0:8]
+Y = array[:,8]
+binarizer = Binarizer(threshold=0.0).fit(X)
+binaryX = binarizer.transform(X)
+# summarize transformed data
+numpy.set_printoptions(precision=3)
+print(binaryX[0:5,:])
